43d99106e157f6bf3aba3b36cc3614515ec86787
Cosmos Emissary <93875236+cosmos-emissary@users.noreply.github.com>
2024-10-22 20:38:14 +0300

    Update winstall.sh

--- WaybarConfig/winstall.sh
+++ WaybarConfig/winstall.sh
@@ -1,31 +1,34 @@
 #!/bin/bash
-echo "Transformation begins."
+echo "Transformation begins. Join the Autobots"
 # Check if "waybar" directory exists in the current directory
 if [ -d "waybar" ]; then
-    # Check if "waybar" directory exists under .config
-    if [ -d "$HOME/.config/waybar" ]; then
-        # If it exists, delete it
-        echo "First, deleting $HOME/.config/waybar directory..."
-        rm -rf "$HOME/.config/waybar"
-    fi
-    # Copy "waybar" directory
-    echo "Copying waybar directory to $HOME/.config/..."
-    cp -r "waybar" "$HOME/.config/"
-    echo "Copying completed."
-
-    # Check if Waybar service is running
-    if pgrep -x "waybar" > /dev/null; then
-        # If it's running, stop it
-        echo "Stopping Waybar service..."
-        pkill -x "waybar"
-        sleep 1  # Wait for a moment to ensure the stopping process is complete
-    fi
-
-    # Start Waybar service
-    echo "Starting Waybar service..."
-    waybar &  # Run in the background
-else
-    # If "waybar" directory is not found, display error message
-    echo "Error: 'waybar' directory not found in this location. Operation canceled."
-    exit 1
+# Check if "waybar" directory exists under .config
+if [ -d "$HOME/.config/waybar" ]; then
+# If it exists, delete it
+echo "First, deleting $HOME/.config/waybar directory..."
+rm -rf "$HOME/.config/waybar"
 fi
+# Copy "waybar" directory
+echo "Copying waybar directory to $HOME/.config/..."
+cp -r "waybar" "$HOME/.config/"
+echo "Copying completed."
+
+
+# Check if Waybar service is running
+if pgrep -x "waybar" > /dev/null; then
+# If it's running, stop it
+echo "Stopping Waybar service..."
+pkill -x "waybar"
+sleep 1 # Wait for a moment to ensure the stopping process is complete
+fi
+
+
+# Start Waybar service
+echo "Starting Waybar service..."
+waybar & # Run in the background
+else
+# If "waybar" directory is not found, display error message
+echo "Error: 'waybar' directory not found in this location. Operation canceled."
+exit 1
+fi
+
